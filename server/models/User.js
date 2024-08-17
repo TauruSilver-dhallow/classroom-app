1ce4975a5cbe95786bfa87a6bee6c6a280136391
taurussilver100207 <110682916+taurussilver100207@users.noreply.github.com>
2024-08-17 15:36:13 +0700

    auth backend

---
+++ server/models/User.js
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+import validator from "validator";
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        max: [50, "The first name cannot exceed 50 characters"]
+    },
+    lastName: {
+        type: String,
+        required: true,
+        max: [50, "The last name cannot exceed 50 characters"]
+    },
+    email: {
+        type: String,
+        required: true,
+        max: 50,
+        unique: true,
+        lowercase: true,
+        validate: {
+            validator: v => {
+                return validator.isEmail(v)
+            },
+            message: props => `${props.value} is not a valid email address.`
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        min: [8, "The password must have at least 6 characters"],
+        max: [30, "The password can not exceed 30 characters"],
+        validate: {
+            validator: v => {
+                const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/
+                return specialCharacters.test(v)
+            },
+            message: props => `${props.value} must contain at least one special character.`
+        }
+    },
+    role: {
+        type: String,
+        enum: ["student", "teacher"],
+        default: "candidate",
+        required: true,
+        message: '{VALUE} is not supported'
+    },
+    avatarPath: {
+        type: String,
+        default: ""
+    }
+}, { timestamps: true })
+
+const User = mongoose.model("User", UserSchema)
+export default User
